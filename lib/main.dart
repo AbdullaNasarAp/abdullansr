4a8b241a5c16ec988e3c4bc74712b31caa155629
AbdullaNasarAp <abdullansr.connect@gmail.com>
2025-05-05 19:22:17 +0530

    Add GitHub Actions workflow

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:abdullansr/app/app.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart'; // Import flutter_riverpod
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart'; // Import flutter_riverpod
 
 void main() {
+  setUrlStrategy(PathUrlStrategy());
   runApp(ProviderScope(child: const PortfolioApp()));
 }

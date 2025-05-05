a1ad3491b4b4ca635997789553dbb367c3dc1d8c
AbdullaNasarAp <abdullansr.connect@gmail.com>
2025-05-05 19:59:57 +0530

    Deploy updated Flutter web build

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -45,7 +45,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "77c24485f6416d939d254a50d3b2e725
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "d86816c8344520286486b6151e7ecaad",
+"flutter_bootstrap.js": "e23b4fdf50476c159597f89322760b3b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
